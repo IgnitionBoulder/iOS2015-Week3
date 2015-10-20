9c89809f3016a0dc1467a42228c7b42578e97273
KC <chocokrizx@gmail.com>
2015-10-20 15:40:54 -0600

    One page app for week 3

---
+++ deliverables/ShoppingListHW/ShoppingList/ListItem.swift
@@ -0,0 +1,14 @@
+//
+//  ListItem.swift
+//  Shopping List
+//
+//  Created by Krizia Conrad on 10/19/15.
+//  Copyright © 2015 Krizia Conrad. All rights reserved.
+//
+
+import Foundation
+
+struct ListItem {
+    let name: String
+    var quantity: Int
+}
